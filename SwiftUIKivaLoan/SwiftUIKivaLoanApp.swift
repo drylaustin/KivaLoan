3bab2e38f1bb523000ef5664006c8153eab1d63c
Daryl Agustin <darylagustin@DARYLs-MacBook-Air.local>
2021-11-23 16:55:13 +0800

    First Commit

---
+++ SwiftUIKivaLoan/SwiftUIKivaLoanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIKivaLoanApp.swift
+//  SwiftUIKivaLoan
+//
+//  Created by DARYL AGUSTIN on 10/1/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIKivaLoanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
